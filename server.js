4fd806b34de923feebf56d3ab1be2179c970e36b
ShashankVenkatesh <shashankvenkatesh7906@gmail.com>
2025-01-29 11:23:04 +0530

    hello

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.get('/', (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log(`Server is running on port http://localhost:${port}.`));
+app.listen(3000, () => console.log(`Server is running on port http://localhost:3000`));
